a05ab3611c1afb5efd99b510452e303504ccbdaa
Wasserwecken <eric.dolch@gmail.com>
2020-03-09 20:26:26 +0100

    changep attern layout

--- glsl/textures/colortestpattern.glsl
+++ glsl/textures/colortestpattern.glsl
@@ -12,7 +12,7 @@ vec3 pattern1(vec2 uv)
 
 
     // full color
-    float line = floor(uv.x * 9.0);
+    float line = floor(uv.x * 8.0);
     float red = mod(line, 2.0);
     float green = mod(line * 0.5, 2.0);
     float blue = mod(line * 0.25, 2.0);
@@ -39,10 +39,10 @@ vec3 pattern1(vec2 uv)
     //contrast steped
     line = floor(uv.y * 12.0);
     float steps = 20.0;
-    float is_contrast_smooth = step(7.0, line) * step(line, 7.0);
-    float is_contrast_stepped = step(4.0, line) * step(line, 4.0);
+    float is_contrast_smooth = step(6.0, line) * step(line, 6.0);
+    float is_contrast_stepped = step(5.0, line) * step(line, 5.0);
 
-    result = is_contrast_smooth * (vec3(1.0 - uv.x))
+    result = is_contrast_smooth * (vec3(uv.x))
             + (1.0 - is_contrast_smooth) * result;
     result = is_contrast_stepped * (vec3(floor(uv.x * steps) / (steps - 1.0)))
             + (1.0 - is_contrast_stepped) * result;
